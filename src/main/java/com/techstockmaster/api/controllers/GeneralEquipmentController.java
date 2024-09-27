ed5592459241d3e1c91e9abb9c1ffe3bbd8b7837
arthur-paraibano <arthurmik860@gmail.com>
2024-09-27 09:19:53 -0300

    Atualizando os perfis de dev e prod e implementando método de integração de novos equipamentos a partir da Lykos

--- src/main/java/com/techstockmaster/api/controllers/GeneralEquipmentController.java
+++ src/main/java/com/techstockmaster/api/controllers/GeneralEquipmentController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,40 +31,59 @@ public class GeneralEquipmentController {
     }
 
     @GetMapping("/all")
-    @Operation(summary = "Listar todos os usuários", description = "Retorna uma lista de todos os usuários")
+    @Operation(summary = "Listar todos os equipamentos", description = "Retorna uma lista de todos os equipamentos")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de usuários retornada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado"),
+            @ApiResponse(responseCode = "200", description = "Lista de equipamentos retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhum equipamento encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
     })
     public ResponseEntity<List<GeneralEquipmentModal>> getAll() {
-        List<GeneralEquipmentModal> usersList = service.findAll();
-        if (!usersList.isEmpty()) {
-            for (GeneralEquipmentModal user : usersList) {
-                Integer id = user.getId();
-                user.add(linkTo(methodOn(GeneralEquipmentController.class).getById(user.getId())).withSelfRel());
+        List<GeneralEquipmentModal> equipmentList = service.findAll();
+        if (!equipmentList.isEmpty()) {
+            for (GeneralEquipmentModal equipment : equipmentList) {
+                equipment.add(linkTo(methodOn(GeneralEquipmentController.class).getById(equipment.getId())).withSelfRel());
             }
+            return ResponseEntity.ok().body(equipmentList);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return ResponseEntity.ok().body(usersList);
     }
 
-
     @GetMapping("/{id}")
-    @Operation(summary = "Buscar usuário por ID", description = "Retorna um usuário específico baseado no ID fornecido")
+    @Operation(summary = "Buscar equipamento por ID", description = "Retorna um equipamento específico baseado no ID fornecido")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "200", description = "Equipamento encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Equipamento não encontrado"),
             @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
     })
     public ResponseEntity<GeneralEquipmentModal> getById(Integer id) {
-        GeneralEquipmentModal user = service.findById(id);
-        if (user != null) {
-            user.add(linkTo(methodOn(GeneralEquipmentController.class).getAll()).withRel("All General Equipment"));
-            return ResponseEntity.status(200).body(user);
-        } else {
-            return ResponseEntity.status(404).build();
+        try {
+            GeneralEquipmentModal equipment = service.findById(id);
+            if (equipment != null) {
+                equipment.add(linkTo(methodOn(GeneralEquipmentController.class).getAll()).withRel("All General Equipment"));
+                return ResponseEntity.status(HttpStatus.OK).body(equipment);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-
+    @GetMapping("/add")
+    @Operation(summary = "Executar a integração de equipamentos", description = "Executa a integração entre a base Lykos e o banco de dados local")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Integração realizada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor durante a integração")
+    })
+    public ResponseEntity<String> add() {
+        try {
+            int rowsProcessed = service.executeIntegration();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Integração concluída com sucesso. Equipamentos adicionados: " + rowsProcessed);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro durante a integração: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/techstockmaster/api/services/GeneralEquipmentService.java
+++ src/main/java/com/techstockmaster/api/services/GeneralEquipmentService.java
@@ -3,5 +3,8 @@ package com.techstockmaster.api.services;
 import com.techstockmaster.api.controllers.dtos.GeneralEquipmentDto;
 import com.techstockmaster.api.domain.models.GeneralEquipmentModal;
 
+import java.sql.SQLException;
+
 public interface GeneralEquipmentService extends CrudService<GeneralEquipmentModal, Integer, GeneralEquipmentDto> {
+    int executeIntegration() throws SQLException;
 }
