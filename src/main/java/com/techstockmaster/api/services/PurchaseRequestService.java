f77f6f7a4c6a59057bae35bf00b4caa4cc636fd9
arthur-paraibano <arthurmik860@gmail.com>
2024-10-24 13:49:31 -0300

    Metodos "PurchaseRequest" já existia com outro nome

--- src/main/java/com/techstockmaster/api/services/PurchaseRequestService.java
+++
@@ -1,7 +0,0 @@
-package com.techstockmaster.api.services;
-
-import com.techstockmaster.api.controllers.dtos.PurchaseRequestDto;
-import com.techstockmaster.api.domain.models.PurchaseRequestModel;
-
-public interface PurchaseRequestService extends CrudService<PurchaseRequestModel, Integer, PurchaseRequestDto> {
-}
